bb34ecd653f0e924b979ea461469f098fd82cc89
teresaupdyke <garner@ccpo.odu.edu>
2018-07-17 15:19:56 +0000

    update aispatt

--- matlab/user_contrib/aispatt.m
+++ matlab/user_contrib/aispatt.m
@@ -14,6 +14,21 @@
 %   7. Loop Data Filtering *This is where the most adjusting is done!*
 %   8. Statistics
 %   9. Plotting (May need to tweak some plot window values, etc.)
+%
+% 2018-07-17 (tgu) Modifications to put notes in the files, add header info, 
+%                  exclude NaNs from the output patt file, distribute figures 
+%                  on the display, print images, see "TGU" comments below
+
+% NOTES: The filtering is based on column numbers not column header names.
+% Doublecheck that these numbers are correct for your version of the loop
+% file.  It works for %TableType: LOOP LP05 but other versions require
+% checking.
+%
+% Before running aispatt.m sections, you will need to do the following:
+%
+%    edit the reference .patt filename and the LOOP folder name in section 1
+%    edit the site info (code, ant1bearing and frequency) in section 2
+%    edit filter string construction in section 7
 
 clear
 
@@ -21,13 +36,12 @@ clear
 
 % Data file locations
 % If no patt file to compare, just make pattfile string empty:
-pattfile = '/Users/chad/Desktop/MONT_APM_Tests_20180507/Boat/PatternData/PATT_MONT_2018_05_09_1444_01d00sm.xlp/SEAS_MONT_2018_05_09_1444.patt';
+pattfile = '/Users/teresa/Desktop/Codar_Files/Pattern_Measurements/LISL/All_Loops/PATT_LISL_20180419_20180623_CLP_smooth00_r01_i01.xlp/SEAS_LISL_2018_06_22_0000.patt';
 
-loopfolder = '/Users/chad/Desktop/MONT_APM_Tests_20180507/AutoAPM_rev1/PatternData/Loops/';
-
+loopfolder = '/Users/teresa/Desktop/Codar_Files/Pattern_Measurements/LISL/All_Loops/';
 
 % Load Pattern File
-if (~isempty(pattfile)),
+if (~isempty(pattfile))
     patt = load(pattfile);
 else
     patt = [];
@@ -40,31 +54,33 @@ end
 % loop = load([loopfolder,loopfile]);
 
 disp('Loading Loop Files...');
-
+tic
 % Load & concatenate all loop file data
 loop = [];
 d = dir([loopfolder,'*.loop']);
 for n = 1:length(d), loop = cat(1,loop,load([loopfolder,d(n).name]));end
+toc
 
 %% 2. Set Parameters
 
 % Choose Site
-code = 'MONT';
+code = 'LISL';
 
 % Set Parameters based on site
 
-% % ************
-% % *** MONT ***
-% % ************
-% %
-
 % Site Info
-ant1bearing = 298;
-txfreq = 13.500000;
+lat = 36.69172;
+lon = -75.92263;
+ant1bearing = 130;
+txfreq = 4.537183;
 
 % Data & plot parameters
-start = 170;
-stop = 355;
+% not filtering on these for .patt unless specified in the filter section below
+% keep sv copies of original values that are not "wrapped to 180" for descriptive notes
+start = 352;  svstart = start;  %degrees True
+stop = 149;   svstop = stop;    %degrees True
+printplots = 1;  % TGU set to 1 to save figures as PNG images, otherwise 0
+
 
 %% 3
 plotstart = 30*floor(start/30);
@@ -80,7 +96,7 @@ braggvel = sqrt(9.8*braggwavelength/(2*pi));  % Bragg Velocity (m/s)
 
 %% 4. Reorganize Loop Data
 
-if (start>stop),
+if (start>stop)
     disp('Wrapping to 180...');
     loop(:,1) = wrapTo180(loop(:,1));
     start = wrapTo180(start);
@@ -91,7 +107,7 @@ end
 
 bearings = (start:brngIncr:stop)';
 
-if (~isempty(patt)),
+if (~isempty(patt))
     patt(:,1) = ant1bearing + patt(:,1);
     % patt(:,1) = mod(patt(:,1),360);
     patt = sortrows(patt,1);
@@ -110,23 +126,31 @@ loop(:,35:37) = [floor(loop(:,33)/10000) (mod(loop(:,33),10000)-mod(loop(:,33),1
 loop(:,38:40) = [floor(loop(:,34)/10000) (mod(loop(:,34),10000)-mod(loop(:,34),100))/100 mod(loop(:,34),100)];
 loop(:,41) = datenum(loop(:,35:40));
 
+ds = min(loop(:,41));
+de = max(loop(:,41));
+
+%pattfolder = sprintf('PATT_%s_%s_%s/',code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd'));
+pattfolder = sprintf('PATT_%s_%s_%s_P%s/',code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd'),datestr(now,'yyyymmddHHMM'));
+eval(sprintf('mkdir %s%s',loopfolder,pattfolder(1:end-1)))  % TGU create directory to save .patt file and images
+
+
 %% 7. Loop Data Filtering
 
 disp('Filtering Loop Data...');
 
 % Filter values
-minSNR = 11; % Minimum Signal-to-Noise Threshold
+minSNR = 12; % Minimum Signal-to-Noise Threshold
 maxSNR = 40; % Maximum Signal-to-Noise Threshold
 maxRangeWidth = 3;
 minRangeWidth = 1;
 minDoppWidth = 1;
 maxDoppWidth = 20;
-maxRange = 50; % Maximum Range to Vessel (km)
-minRange = 3; % Minimum Range to Vessel (km)
+maxRange = 80; % Maximum Range to Vessel (km)
+minRange = 10; % Minimum Range to Vessel (km)
 maxcurrent = 1; % Maximum absolute current speed (m/s)
 minVesselSpeed = .5;
 maxVesselSpeed = 13;
-minEchoes = 4;
+minEchoes = 1;
 
 % use subsampling?
 dosubsample = 0; % logical: 1 = yes, 0 = no
@@ -135,78 +159,99 @@ numsamples = 200; % set lower than the size of the set
 % Begin assembling filter string statement
 filtStr = 'ind = find(';
 
+notes = sprintf('%s_to_%s;',datestr(ds,'yyyy-mm-dd'), datestr(de,'yyyy-mm-dd'));
+
+%____________________________________________________________________________________
+%TGU NOTE - HAVE TO USE AT LEAST ONE AND NOT MORE THAN ONE OF THESE FIRST THREE FILTERS
+%DEALING WITH MINIMUM SIGNAL TO NOISE
+
 % Require minSNR for Ch3 only
-% filtStr = [filtStr,'loop(:,14)>minSNR'];
+%filtStr = [filtStr,'loop(:,14)>minSNR']; notes = [notes, sprintf('monopole_only_SNR>%2d;',minSNR)];
 
 % Require minSNR for Ch3 & either Ch1 or Ch2
-filtStr = [filtStr,'loop(:,14)>minSNR & (loop(:,12)>minSNR | loop(:,13)>minSNR)'];
+filtStr = [filtStr,'loop(:,14)>minSNR & (loop(:,12)>minSNR | loop(:,13)>minSNR)'];   notes = [notes, sprintf('monopole_&_a_loop_SNR>%02d;',minSNR)];
+
+% Require minSNR for all three channels
+%filtStr = [filtStr,'loop(:,14)>minSNR & loop(:,12)>minSNR & loop(:,13)>minSNR']; notes = [notes, sprintf('all_channels_SNR>%02d;',minSNR)];
+
+%_____________________________________________________________________________________
 
 % Require maxSNR for Ch3, Ch1 & Ch2
-% filtStr = [filtStr,'& (loop(:,14)<maxSNR & loop(:,12)<maxSNR & loop(:,13)<maxSNR)'];
-
-% Require minSNR for Ch3, Ch1 & Ch2
-% filtStr = [filtStr,'loop(:,14)>minSNR & loop(:,12)>minSNR & loop(:,13)>minSNR'];
+% filtStr = [filtStr,' & loop(:,14)<maxSNR & loop(:,12)<maxSNR & loop(:,13)<maxSNR']; notes = [notes, sprintf('all_channels_SNR<%02d;',maxSNR)];
 
 % Require min IIR SNR for all three channels
-% filtStr = [filtStr,'loop(:,27)>minSNR & loop(:,28)>minSNR & loop(:,29)>minSNR'];
+% TGU NOTE had to increase column numbers by 1 and added &
+%filtStr = [filtStr,' & loop(:,28)>minSNR & loop(:,29)>minSNR & loop(:,30)>minSNR']; notes = [notes, sprintf('all_channels_IIR_SNR>%02d;',minSNR)];
 
 % Require min IIR SNR for Ch 3 & either Ch1 or Ch2
-% filtStr = [filtStr,'loop(:,27)>minSNR & (loop(:,28)>minSNR | loop(:,29)>minSNR)'];
+%TGU NOTE corrected Ch 3 column number to 30 and added &
+%filtStr = [filtStr,' & loop(:,30)>minSNR & (loop(:,28)>minSNR | loop(:,29)>minSNR)']; notes = [notes, sprintf('monopole_&_a_loop_IIR_SNR>%02d;',minSNR)];
+
 
 % Filter out solutions outside of bearing limits
-% if (start < stop),
-%     filtStr = [filtStr,' & loop(:,1)>=start & loop(:,1)<=stop'];
-% else
-%     filtStr = [filtStr,' & loop(:,1)>=start | loop(:,1)<=stop'];
-% end
+if (start < stop)
+    filtStr = [filtStr,' & loop(:,1)>=start & loop(:,1)<=stop']; notes = [notes, sprintf('bearing>=%03d_and_bearing<=%03d;',svstart,svstop)];
+else
+    filtStr = [filtStr,' & loop(:,1)>=start | loop(:,1)<=stop']; notes = [notes, sprintf('bearing>=%03d_or_bearing<=%03d;',svstart,svstop)];
+end
 
 % Filter out solutions in an arbitrary section
-% filtStr = [filtStr,' & ~(loop(:,1)<180 & real(loopA13)>.5)'];
+%cf1 = 180; cf2 = 0.5; filtStr = [filtStr,' & ~(loop(:,1)<cf1 & real(loopA13)>cf2)'];  notes = [notes, sprintf('custom_filter_removes_bearings_that_are_<%03d_with_real_loop1_amplitude<%03.1f;',cf1,cf2)];
 
 % % Filter out solutions near DC
-filtStr = [filtStr,' & abs(loop(:,9)) > minVesselSpeed'];
-% 
+%filtStr = [filtStr,' & abs(loop(:,9)) >minVesselSpeed'];  notes = [notes, sprintf('Bragg_velocity>%04.1f;',minVesselSpeed)];
+ 
+
 % % Filter out solutions with high radial velocity
-% filtStr = [filtStr,' & abs(loop(:,9)) < maxVesselSpeed'];
+% filtStr = [filtStr,' & abs(loop(:,9)) < maxVesselSpeed']; notes = [notes, sprintf('Bragg_velocity<%03d;',maxVesselSpeed)];
 % 
 % Filter out solutions past max range
-% filtStr = [filtStr,' & abs(loop(:,8))/1000 < ', num2str(maxRange)];
+ filtStr = [filtStr,' & abs(loop(:,8))/1000 < ', num2str(maxRange)];  notes = [notes, sprintf('range<%04.1fkm;',maxRange)];
 
 % Filter out solutions closer than min range
-% filtStr = [filtStr,' & abs(loop(:,8))/1000 > ', num2str(minRange)];
+% filtStr = [filtStr,' & abs(loop(:,8))/1000 > ', num2str(minRange)];  notes = [notes, sprintf('range>%04.1fkm;',minRange)];
 
 % Filter out solutions in a certain sector
-% filtStr = [filtStr,' & ~(loop(:,8) > 27.5 & loop(:,1) > 93 | loop(:,1) < 300)'];
+% cf1 = 27.5; cf2 = 92; cf3 = 300; filtStr = [filtStr,' & ~(loop(:,8) > cf1 & loop(:,1) > cf2 | loop(:,1) < cf3)']; notes = [notes, sprintf('custom_filter_removes_range>%04.1fkm_and_bearing>%03d_or_bearing_<%03d;',cf1,cf2,cf3)];
 
 % Filter out solutions with peaks wide in range
-filtStr = [filtStr,' & loop(:,25) - loop(:,24) <= maxRangeWidth'];
+% filtStr = [filtStr,' & loop(:,25) - loop(:,24) <= maxRangeWidth'];  notes = [notes, sprintf('range_width<=%04.1fkm;',maxRangeWidth)];
 
 % Filter out solutions with peaks narrow in range
-% filtStr = [filtStr,' & (loop(:,25) - loop(:,24) >= minRangeWidth)'];
+% filtStr = [filtStr,' & (loop(:,25) - loop(:,24) >= minRangeWidth)']; notes = [notes, sprintf('range_width>=%04.1fkm;',minRangeWidth)];
 
 % Filter out solutions with peaks wide in Doppler
-filtStr = [filtStr,' & loop(:,27) - loop(:,26) <= maxDoppWidth'];
+%TGU NOTE, 27 - 26 is correct column assignment for my files
+%filtStr = [filtStr,' & loop(:,27) - loop(:,26) <= maxDoppWidth']; notes = [notes, sprintf('Doppler_width<=%02d;',maxDoppWidth)];
 % filtStr = [filtStr,' & loop(:,26) - loop(:,25) <= maxDoppWidth'];
 
 % Filter out solutions by date range
-% filtStr = [filtStr,' & (loop(:,40) >= datenum(2015,10,19,0,0,0) & loop(:,40) <= datenum(2015,10,26,0,0,0))'];
-% filtStr = [filtStr,' & loop(:,40) >= datenum(2014,10,16,0,0,0)'];
+%cds = datenum(2015,10,19,0,0,0); cde = datenum(2015,10,26,0,0,0); filtStr = [filtStr,' & (loop(:,40) >= cds & loop(:,40) <= cde)'];  notes = [notes, sprintf('custom_dates_%s_to_%s;',datestr(cds,'yyyy-mm-dd'), datestr(cde,'yyyy-mm-dd'))];
 
 % Filter out solutions by hour of the day
-% filtStr = [filtStr,' & loop(:,37) < 15'];
-% filtStr = [filtStr,' & loop(:,38) < 8 | loop(:,38) > 20'];
+% hstart = 13; hstop = 20;
+%  if (hstart < hstop)
+%     filtStr = [filtStr,' & loop(:,38) > hstart & loop(:,38) < hstop']; notes = [notes, sprintf('hour>%02d_and_hour<%02d;',hstart,hstop)];
+%  else
+%      filtStr = [filtStr,' & loop(:,38) > hstart | loop(:,38) < hstop']; notes = [notes, sprintf('hour>%02d_or_hour<%02d;',hstart,hstop)];
+%  end
+
 
 % Filter out solutions near Bragg
-filtStr = [filtStr,' & ~(abs(loop(:,9))>braggvel-maxcurrent & abs(loop(:,9))<braggvel+maxcurrent)'];
+%filtStr = [filtStr,' & ~(abs(loop(:,9))>braggvel-maxcurrent & abs(loop(:,9))<braggvel+maxcurrent)']; notes = [notes, sprintf('velocities>=%04.1fm/s_from_Bragg_vel;',maxcurrent)];
 
 % Limit Solutions to Doppler between Bragg & DC
-% filtStr = [filtStr,' & abs(loop(:,9))<braggvel-maxcurrent'];
+% filtStr = [filtStr,' & abs(loop(:,9))<braggvel-maxcurrent']; notes = [notes, sprintf('absolute_velocity<%04.1fm/s;',braggvel - maxcurrent)];
+
 
 % Limit Solutions to specific MMSI
-% filtStr = [filtStr,' & loop(:,30)==351176000'];
+% TGU NOTE had to increase column number by 1
+% mmsi = 351176000; filtStr = [filtStr,' & loop(:,31)== mmsi']; notes = [notes, sprintf('MMSI=%s;',num2str(mmsi))];
+
 
 % Close the filter string
 filtStr = [filtStr,');'];
+notes = [notes, sprintf('min_%d_echoes_at_bearing',minEchoes)]
 
 % Evaluate Filter String
 eval(filtStr);
@@ -217,7 +262,7 @@ eval(filtStr);
 
 % trgtBrngsReduced = round(loop(:,1)/brngIncr)*brngIncr;
 
-if (dosubsample && numsamples < length(ind)),
+if (dosubsample && numsamples < length(ind))
     sampind = randperm(length(ind),numsamples)';
 else
     sampind = (1:length(ind))';
@@ -231,14 +276,14 @@ loopRIwmean = NaN*ones(length(bearings),4);
 loopRIstd = NaN*ones(length(bearings),4);
 
 % Loop through bearings & antenna parameters(4)
-for m = 1:length(bearings),
+for m = 1:length(bearings)
     
 %     indb = find(trgtBrngsReduced(ind,1) == bearings(m));
     indb = find(loop(ind(sampind),1) > bearings(m)-brngwidth/2 & loop(ind(sampind),1) < bearings(m)+brngwidth/2); % Need to address wrap around North
     numPerBrng(m) = length(indb);
 %     meanSNR(m) = mean(loop(ind(indb),27));
-    if numPerBrng(m) > minEchoes,
-        for n = 1:4,
+    if numPerBrng(m) > minEchoes
+        for n = 1:4
             
             % Mean
             loopRImean(m,n) = mean(loopRI(ind(sampind(indb)),n));
@@ -256,10 +301,12 @@ for m = 1:length(bearings),
     end
 end
 
-
+notnan = find(~isnan(loopRImedian(:,1))); %TGU added to adjust table row count for written file
+bcount = length(notnan); %TGU added
 
 
 %% 9. Plotting
+
 disp('Plotting.')
 % plotcolors = ['r','y','b','g'];
 labelfontsize = 14;
@@ -272,12 +319,12 @@ figNameStr = [code,', Min SNR ',num2str(minSNR)];
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Figure 1. Real & Imaginary components
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-figure('Name',figNameStr)
+fw = 560; fh = 420;
+figure('Name',figNameStr,'Position',[100,100,fw,fh])
 clf
 
 % Plot Real and Imaginary Antenna Patterns
-for n = 1:4,
+for n = 1:4
     subplot(2,2,n)
 %     plot(loop(ind,1),loopRI(ind,n),[plotcolors(n),'.'])
     plot(loop(ind(sampind),1),loopRI(ind(sampind),n), 'r.', 'markersize', 8)
@@ -298,12 +345,13 @@ for n = 1:4,
     xlabel('Bearing (deg True)','Fontsize', labelfontsize)
 end
 
-
-
+if printplots
+  eval(sprintf('print -dpng %s%sPATT_%s_%s_%s_RealImag.png',loopfolder,pattfolder, code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd') ) )
+end
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 2. Polar plot of ant patt amplitude
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure
+figure('Position',[100+fw,100,fw,fh])
 
 polamp1 = abs(loopRImedian(:,1)+1i*loopRImedian(:,2));
 polamp2 = abs(loopRImedian(:,3)+1i*loopRImedian(:,4));
@@ -323,16 +371,18 @@ polargeo(bearings*pi/180,polamp2,'bo')
 % polargeo(bearings*pi/180,abs(loopRIwmean(:,3)+1i*loopRIwmean(:,4)),'cx')
 
 % Baseline pattern
-if(~isempty(patt)),
+if(~isempty(patt))
     polargeo(patt(:,1)*pi/180,patt(:,7),'m--')
     polargeo(patt(:,1)*pi/180,patt(:,9),'c--')
 end
-
+if printplots
+  eval(sprintf('print -dpng %s%sPATT_%s_%s_%s_AmpPolar.png',loopfolder,pattfolder, code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd') ) )
+end
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 3. Cartesian plot of ant patt phases
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure 
+figure('Position',[100+2*fw,100,fw,fh])
 
 % Median
 plot(bearings,angle(loopRImedian(:,1)+1i*loopRImedian(:,2))*180/pi,'ro')
@@ -348,7 +398,7 @@ set(gca,'YTick',-180:30:180);
 % plot(bearings,angle(loopRIwmean(:,3)+1i*loopRIwmean(:,4))*180/pi,'cx')
 
 % Baseline pattern
-if(~isempty(patt)),
+if(~isempty(patt))
     plot(patt(:,1),patt(:,8),'m--')
     plot(patt(:,1),patt(:,10),'c--')
 end
@@ -358,11 +408,13 @@ end
 % axis(a);
 xlabel('Bearing (deg True)','Fontsize', labelfontsize)
 ylabel('Phase (deg)','Fontsize', labelfontsize)
-
+if printplots
+  eval(sprintf('print -dpng %s%sPATT_%s_%s_%s_Phase.png',loopfolder, pattfolder,code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd') ) )
+end
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 4. Cartesian plot of ant patt amps
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure 
+figure('Position',[100,100+fh+75,fw,fh])
 
 % Median
 plot(bearings,abs(loopRImedian(:,1)+1i*loopRImedian(:,2)),'ro')
@@ -375,7 +427,7 @@ set(gca,'XTick',plotstart:30:plotstop);
 % plot(bearings,abs(loopRIwmean(:,3)+1i*loopRIwmean(:,4))*180/pi,'cx')
 
 % Baseline pattern
-if(~isempty(patt)),
+if(~isempty(patt))
     plot((patt(:,1)),patt(:,7),'m--')
     plot((patt(:,1)),patt(:,9),'c--')
 end
@@ -384,10 +436,14 @@ xlim([plotstart plotstop]);
 xlabel('Bearing (deg True)','Fontsize', labelfontsize)
 ylabel('Amplitude Ratio','Fontsize', labelfontsize)
 
+if printplots
+eval(sprintf('print -dpng %s%sPATT_%s_%s_%s_Amplitude.png',loopfolder,pattfolder, code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd') ) )
+end
+
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 5. Plot Metadata
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure
+figure('Position',[100+fw,100+fh+75,fw,fh])
 clf
 
 % Plot range vs. bearing
@@ -473,40 +529,50 @@ xlim([plotstart plotstop]);
 set(gca,'XTick',plotstart:30:plotstop);
 title('f)');
 
+if printplots
+eval(sprintf('print -dpng %s%sPATT_%s_%s_%s_Metadata.png',loopfolder,pattfolder, code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd') ) )
+end
+
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 6. Plot Vessel Positions
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure
+figure('Position',[100+fw*2,100+fh+75,fw,fh])
 polargeo(pi,60);
 hold on;
 polargeo(loop(ind(sampind),1)*pi/180, loop(ind(sampind),8)/1000, 'r.');
+if printplots
+eval(sprintf('print -dpng %s%sPATT_%s_%s_%s_Vessels.png',loopfolder,pattfolder,code, datestr(min(loop(:,41)),'yyyymmdd'),datestr(max(loop(:,41)),'yyyymmdd') ) )
+end
 
 %% 10. Write Pattern File
 
 filename = ['SEAS_' code datestr(max(loop(:,41)),'_yyyy_mm_dd_HHMM') '.patt'];
-fileID = fopen(filename,'w');
+fileID = fopen([loopfolder,pattfolder,filename],'w'); %TGU save .patt file in a pattern folder under the loops folder
 
 fprintf(fileID,'%%CTF: 1.00\n');
 fprintf(fileID,'%%%% SeaSonde Crossloop Antenna Pattern File\n');
 fprintf(fileID,'%%FileType: PATT xlp4 "AntennaPattern"\n');
 fprintf(fileID,'%%Owner: CODAR Ocean Sensors\n');
 fprintf(fileID,['%%TimeStamp: ',datestr(max(loop(:,41)),'yyyy mm dd  HH MM SS'),'\n']);
-fprintf(fileID,'%%Site: LISL ""\n');
-% fprintf(fileID,'%%Origin:  ',36.6917167  -75.9226333,'\n');
-fprintf(fileID,'%%UserComment: VesselEchoPattern\n');
+fprintf(fileID,'%%Site: %s ""\n',code); %TGU added code variable
+fprintf(fileID,'%%Origin:  %11.7f %11.7f\n',lat,lon);  %TGU uncommented and added requirement for origin in section 2
+fprintf(fileID,['%%UserComment: VesselEchoPattern;',sprintf('%s',notes),'\n']);
 fprintf(fileID,['%%AntennaBearing: ',sprintf('%3.1f',ant1bearing),' True ;;\n']);
 fprintf(fileID,'%%ReferenceChannel: 3\n');
 fprintf(fileID,'%%AngularResolution: 1.0 deg\n');
 fprintf(fileID,'%%TableType: PATT MP01\n');
 fprintf(fileID,'%%TableColumns: 5\n');
 fprintf(fileID,'%%TableColumnTypes: BEAR A13R A13I A23R A23I\n');
-fprintf(fileID,['%%TableRows: ',sprintf('%d',size(bearings,1)),'\n']);
+%fprintf(fileID,['%%TableRows: ',sprintf('%d',size(bearings,1)),'\n']);   
+fprintf(fileID,'%%TableRows: %d\n',bcount);  %TGU added to account for NaN values removed
 fprintf(fileID,'%%TableStart:\n');
 fprintf(fileID,'%%%% Bearing    A13        A13        A23        A23\n');
 fprintf(fileID,'%%%%  DegCW     real       imag       real       imag\n');
 
-for n = 1:size(bearings,1),
-    fprintf(fileID,'  %03.1f  %1.7f  %1.7f  %1.7f  %1.7f\n',bearings(n)-ant1bearing,loopRImedian(n,1),loopRImedian(n,2),loopRImedian(n,3),loopRImedian(n,4));
+for n = 1:size(bearings,1)
+    if ~isnan(loopRImedian(n,1)) %TGU added to keep NaNs out of file
+      fprintf(fileID,'  %03.1f  %1.7f  %1.7f  %1.7f  %1.7f\n',bearings(n)-ant1bearing,loopRImedian(n,1),loopRImedian(n,2),loopRImedian(n,3),loopRImedian(n,4));
+    end
 end
 
 fprintf(fileID,'%%TableEnd:\n');
@@ -515,5 +581,5 @@ fprintf(fileID,['%%ProcessedTimeStamp: ',datestr(now,'yyyy mm dd  HH MM SS'),'\n
 fprintf(fileID,'%%ProcessingTool: "Chad''s MATLAB Script" 2.0\n');
 fprintf(fileID,'%%End:\n');
 
-fclose(fileID)
+fclose(fileID);
 
